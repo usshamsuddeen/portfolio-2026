842278eb1968a9a197f2e23f310834552a231a60
Muhammad Usman Shams <admin@wapify.com>
2026-06-13 14:01:44 +0500

    Fix API proxy in dev environment

--- run.bat
+++ run.bat
@@ -2,6 +2,9 @@
 title Portfolio Server
 cls
 
+echo Starting Backend API Server (Port 8080)...
+start "Backend Server" node server.js
+
 echo Starting Portfolio Development Server...
 echo Opening browser at http://localhost:3000/...
 start http://localhost:3000/
